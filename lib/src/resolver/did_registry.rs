92ab7606c7ddb77f094c7a61e5dbfaffa93e717c
Andrew Plaza <github@andrewplaza.dev>
2024-01-24 15:21:58 -0500

    remove unnecessary todo

--- lib/src/resolver/did_registry.rs
+++ lib/src/resolver/did_registry.rs
@@ -203,7 +203,6 @@ async fn sign_data<M: Middleware>(
 /// Compute the Keccak-256 hash of input bytes.
 ///
 /// Note that strings are interpreted as UTF-8 bytes,
-// TODO: Add Solidity Keccak256 packing support
 pub fn keccak256<T: AsRef<[u8]>>(bytes: T) -> [u8; 32] {
     let mut output = [0u8; 32];
 
